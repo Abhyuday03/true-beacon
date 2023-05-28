3893af58cea8486435cbb182edf3d8a7d2ceb733
Abhyuday03 <abhyuday@kgpian.iitkgp.ac.in>
2023-05-28 19:39:20 +0530

    delpoy

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 require("dotenv").config();
 const priceRoutes = require("./routes/prices");
@@ -10,6 +11,9 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname + "/public")));
+
 app.use(cors());
 app.use("/api/v1/prices", priceRoutes);
 app.use("/api/v1/user", userRoutes);
